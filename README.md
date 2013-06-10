0593956de969ac16d99bbd58098b3193c7a643bc
Cameron-D <camerondew@live.com>
2013-06-10 21:51:27 +1000

    First version

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 MyBB-Redis-Cache
 ================
 
-A Redis cache handler for MyBB
+A [Redis](http://redis.io/) cache handler for [MyBB](http://www.mybb.com/).
+
+Why?
+----
+
+I currently use Memcache to cache both the MyBB settings and preparsed versions of each post (To save processing time on
+subsequent page loads of posts) however Memcache does not make it easy to clear items if you do not know the name of the
+item (preparsed posts are stored as hashes) so this was built to offer more control over flushing items (And I've always
+wanted an excuse to try Redis)
+
+It may also be an idea to disable persistance for the Redis database (Use it purely as a in-memory cache) as MyBB will
+store persistant caches in its database.
+
+When I understand Redis better I'll add more flexibility to the configuration of it.
+
+Requirements
+----
+
+* A Redis server
+* The [phpredis](https://github.com/nicolasff/phpredis) extension
+
+Installation/Configuration
+----
+
+1. Merge the contents of the repository into your MyBB installation, making sure to overwrite `class_datacache.php`
+
+2. Edit `inc/config.php` and add the following
+
+    ~~~
+    $config['redis']['host'] = '127.0.0.1';
+    $config['redis']['port'] = '6379';
+    ~~~
+
+    Note that the port will default to 6379 if not specified  
+
+3. Enable it by setting 
+    ~~~
+    $config['cache_store'] = 'redis';
+    ~~~
+    in `inc/config.php`
---
+++ inc/cachehandlers/redis.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Redis Cache Handler written for MyBB 1.6 by Cameron:D
+ * 
+ * Repository: https://github.com/Cameron-D/MyBB-Redis-Cache
+ * 
+ * Version 0.9
+ */
+
+class redisCacheHandler
+{
+	/**
+	 * The redis server resource
+	 */
+	public $redis;
+
+	/**
+	 * Unique identifier representing this copy of MyBB
+	 */
+	public $unique_id;
+	
+	function redisCacheHandler($silent=false)
+	{
+		global $mybb;
+		
+		// Check if our DB engine is loaded
+		if(!extension_loaded("redis"))
+		{
+			// Throw our super awesome cache loading error
+			$message = "The phpredis module does not appear to be installed or enabled. It is required to use the redis cachehandler";
+			$error_handler->trigger($message, MYBB_CACHEHANDLER_LOAD_ERROR);
+			die;
+		}
+	}
+
+	/**
+	 * Connect and initialize this handler.
+	 *
+	 * @return boolean True if successful, false on failure
+	 */
+	function connect()
+	{
+		global $mybb, $error_handler;
+		
+		$this->redis = new Redis();
+		
+		if(!$mybb->config['redis']['host'])
+		{
+			$message = "Please configure the redis settings in inc/config.php before attempting to use this cache handler";
+			$error_handler->trigger($message, MYBB_CACHEHANDLER_LOAD_ERROR);
+			die;
+		}
+
+		if(!$mybb->config['redis']['host'])
+		{
+			$mybb->config['redis']['host'] = "11211";
+		}
+
+		try {
+			$this->redis->connect($mybb->config['redis']['host'], $mybb->config['redis']['port']);
+		} catch (RedisException $e) {
+			$message = "Unable to connect to the redis server configured in inc/config.php. Are you sure it is running?";
+			$error_handler->trigger($message, MYBB_CACHEHANDLER_LOAD_ERROR);
+			die;
+		}
+
+		// Set a unique identifier for all queries in case other forums are using the same server
+		$this->unique_id = md5(MYBB_ROOT);
+
+		return true;
+	}
+	
+	/**
+	 * Retrieve an item from the cache.
+	 *
+	 * @param string The name of the cache
+	 * @param boolean True if we should do a hard refresh
+	 * @return mixed Cache data if successful, false if failure
+	 */
+	
+	function fetch($name, $hard_refresh=false)
+	{
+		$data = $this->redis->get("mybb:".$this->unique_id.":".$name);
+
+		if($data === false)
+		{
+			return false;
+		}
+		else
+		{
+			return unserialize($data);
+		}
+	}
+	
+	/**
+	 * Write an item to the cache.
+	 *
+	 * @param string The name of the cache
+	 * @param mixed The data to write to the cache item
+	 * @return boolean True on success, false on failure
+	 */
+	function put($name, $contents)
+	{
+		return $this->redis->set("mybb:".$this->unique_id.":".$name, serialize($contents));
+	}
+	
+	/**
+	 * Delete a cache
+	 *
+	 * @param string The name of the cache
+	 * @return boolean True on success, false on failure
+	 */
+	function delete($name)
+	{
+		if($this->redis->delete("mybb:".$this->unique_id.":".$name))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	/**
+	 * Disconnect from the cache
+	 */
+	function disconnect()
+	{
+		@$this->redis->close();
+	}
+	
+	function size_of($name)
+	{
+		global $lang;
+		
+		return $lang->na;
+	}
+}
+
+?>
